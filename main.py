24c215383b696ffb83f5659d4eb412052a0008b7
hgjk <hjhg>
2019-10-17 20:32:09 -0600

    comentar

--- main.py
+++ main.py
@@ -17,18 +17,18 @@
 
 #Ejercicio #2
 
-from mechanize import Browser
-nombre=str(input("Digite el nombre de la persona que desea buscar \n >"))
-apellido1=str(input("Digite el primer apellido de la persona \n >"))
-apellido2=str(input("Digite el segundo apellido de la persona \n >"))
-browser= Browser()
-browser.open("https://www.consulta.tse.go.cr/consulta_persona/consulta_nombres.aspx")
-form= browser.select_form("form1")
-browser["txtnombre"]=nombre
-browser["txtapellido1"]=apellido1
-browser["txtapellido2"]=apellido2
-response=browser.submit()
-name=nombre
-file=open(name+".html","w")
-file.write(response.read().decode("UTF-8"))
-file.close()
+# from mechanize import Browser
+# nombre=str(input("Digite el nombre de la persona que desea buscar \n >"))
+# apellido1=str(input("Digite el primer apellido de la persona \n >"))
+# apellido2=str(input("Digite el segundo apellido de la persona \n >"))
+# browser= Browser()
+# browser.open("https://www.consulta.tse.go.cr/consulta_persona/consulta_nombres.aspx")
+# form= browser.select_form("form1")
+# browser["txtnombre"]=nombre
+# browser["txtapellido1"]=apellido1
+# browser["txtapellido2"]=apellido2
+# response=browser.submit()
+# name=nombre
+# file=open(name+".html","w")
+# file.write(response.read().decode("UTF-8"))
+# file.close()
